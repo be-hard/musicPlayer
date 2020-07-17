f30c6155c5def1a0241addd9a0e3f8ccdc7e171c
zxy <1329324306@qq.com>
2020-07-17 09:37:47 +0800

    page

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,5 @@ module.exports = {
     chainWebpack: config => {
         config.resolve.alias.set("@", resolve("src"));
     },
-    publicPath: process.env.NODE_ENV === "production" ? "./" : "/"
+    publicPath: process.env.NODE_ENV === "production" ? "/public" : "/"
 };
